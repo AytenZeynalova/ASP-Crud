008a6ad030dc1f99e72531189d5e5e71ee11900a
AytenZeynalova <14rvxpg@code.edu.az>
2022-06-07 12:04:52 +0400

    first commit

---
+++ ASP-Pustok/Areas/Manage/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using ASP_Pustok.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AuthorController : Controller
+    {
+        private readonly PustokDbContext _context;
+
+        public AuthorController(PustokDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Authors.Include(x => x.Books).ToList();
+            return View(data);
+        }
+    }
+}
